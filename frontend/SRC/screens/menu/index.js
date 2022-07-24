e9758b78d3e3241182b4573ee053da4b0cdd6e4b
Bianca Gallicchio <94980037+BiancaGallicchio@users.noreply.github.com>
2022-07-23 23:36:28 -0300

    Update index.js

--- frontend/SRC/screens/menu/index.js
+++ frontend/SRC/screens/menu/index.js
@@ -7,22 +7,35 @@ TouchableOpacity,
 } from 'react-native';
 import { styles } from './styles';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Menu() {
     const navigation = useNavigation();
 return (
     <ImageBackground style={styles.imgBackground}>
     <TouchableOpacity 
-    style={styles.backStyle}
-    onPress={() => navigation.navigate('PrivChat')}
-    >
-        <Text style={styles.textBackStyle}>voltar</Text>
+    style={styles.returnStyle}
+    onPress={() => navigation.navigate('PrivChat')}>
+        <Icon
+          name="chevron-left"
+          size={30}
+          color="#F5ECFA"
+        ></Icon>
     </TouchableOpacity>
     <SafeAreaView style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.buttonStyle}>
+        <TouchableOpacity 
+        style={styles.buttonStyle}
+        onPress={() => navigation.navigate('EditProfile')}>
+          <Text style={styles.textInput}>Editar perfil</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+        style={styles.buttonStyle}
+        /*onPress={() => navigation.navigate('NewContact')}*/>
         <Text style={styles.textInput}>Adicionar nova conversa</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.buttonStyle}>
+        <TouchableOpacity 
+        style={styles.buttonStyle}
+        /*onPress={() => navigation.navigate('NewGroup')}*/>
         <Text style={styles.textInput}>Adicionar novo grupo</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.buttonStyle}>
